02dcce4f7f6a6aa31c9d51d40bf0e988e1de6573
Vared3000 <stashcov5@gmail.com>
2026-03-02 19:47:42 +0300

    test

--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -3,7 +3,7 @@
 export default function GamePage() {
   return (
     <Card>
-      <Typography.Title level={3}>Game</Typography.Title>
+      <Typography.Title level={3}>Game YA TUTU </Typography.Title>
       <Typography.Paragraph>Game page placeholder.</Typography.Paragraph>
     </Card>
   );
